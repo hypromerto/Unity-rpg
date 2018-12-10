ecd8f28e4aa25d7ddcee4e8248fd339275b1bf46
hypromerto <aslan-mert99@hotmail.com>
2018-12-10 17:14:16 +0300

    Added Item and Inventory system

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+    public Item item;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+    }
+
+    void PickUp()
+    {
+        //add to inventory
+        if (Inventory.instance.Add(item) )
+            Destroy(gameObject);
+    }
+}
