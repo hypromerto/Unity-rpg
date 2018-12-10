a2565a29b3dbeb8420adf7ab375f00152a80ab4a
hypromerto <aslan-mert99@hotmail.com>
2018-12-10 16:07:11 +0300

    Initial commit

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    bool isFocus = false;
+    bool hasInteracted = false;
+    Transform player;
+
+    public virtual void Interact()
+    {
+        //This method is to be overwritten
+        
+        Debug.Log("Interacting with: " + transform.name );
+    }
+
+
+
+    void Update()
+    {
+        if ( isFocus && !hasInteracted )
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+
+            if ( distance <= radius )
+            {
+
+                //INTERACT
+
+                Interact();
+                hasInteracted = true;
+            }
+        }
+        
+    }
+
+
+    public void OnFocused( Transform playerTransform )
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+}
